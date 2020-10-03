65a65e3e248f01b5487712924425938e7dd3b287
KlerKpn <59790939+KlerKpn@users.noreply.github.com>
2020-10-03 16:57:21 +0300

    Initial commit

---
+++ scr/model.js
@@ -0,0 +1,8 @@
+export const model = [
+    {type: 'title', value: 'hello'},
+    {type: 'text', value: 'here we go'},
+    {type: 'columns', value: [
+        'ewqeqwe' , 'ewqeq','11111'
+    ]}
+
+]
